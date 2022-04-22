5db32312a78491acf4634b1f8b62658b84df331c
Ubuntu <ubuntu@ip-172-31-49-244.ec2.internal>
2022-04-22 01:18:06 +0000

    Finished code for MagicSquares.java.

--- MagicSquares.java
+++ MagicSquares.java
@@ -1,8 +1,7 @@
 /**
  * The MagicSquares program implements an application that
- * asks the user to choose rock, paper, or scissors, then
- * generates a random selection and tells the user if they
- * have won, lost, or tied.
+ * calls a function which uses recursion to determine all
+ * of the possible magic squares when the magic nuber is 15.
  *
  * @author  Ina Tolo
  * @version 1.0
@@ -45,57 +44,54 @@ class MagicSquares {
     public static final int MAGICNUM = 15;
 
     /**
-     * Constructor.
+     * Empty constructor for the program.
+     *
      */
-    private MagicSquares() {
-        // Prevent instantiation
-        // Optional: throw an exception e.g. AssertionError
-        // if this ever *is* called
-        throw new IllegalStateException("Cannot be instantiated");
-    }
+    MagicSquares() { }
 
     /**
-     * Insert function description.
+     * Generates magic squares.
      *
      * @param square passed in
      * @param currentSquare passed in
      * @param index passed in
      */
-    public static void genSquare(int[] square, int[] currentSquare,
-        int index) {
-        // referenced code from https://www.math.colostate.edu/~betten/courses/M152/LECTURE/lecture9.html
-        for (int counter = 1; counter < 9; counter++) {
-            // System.out.println("test string");
-            
+    public static void genSquare(final int[] square, final int[] currentSquare,
+        final int index) {
+        /* referenced code from
+        math.colostate.edu/~betten/courses/M152/LECTURE/lecture9.html
+        */
+        for (int counter = 0; counter < square.length; counter++) {
+
+            // checks if position in array has been filled
             if (currentSquare[counter] == 0) {
-                currentSquare[counter] = index;
-                square[counter] = 1;
-                
-                if (index < 9) {
+                currentSquare[counter] = 1;
+                square[index] = counter + 1;
+
+                if (index < square.length - 1) {
+                    // calls function with incremented index position
                     genSquare(square, currentSquare, index + 1);
                 } else {
-                    final boolean magicCheck = isMagic(square);
-                    if (magicCheck == true) {
-                        printMagicSquare(currentSquare);
-                    } else {
-                        // index = 0;
+                    // checks if array is a magic square
+                    if (isMagic(square)) {
+                        printMagicSquare(square);
                     }
                 }
+                // resets position to be empty
                 currentSquare[counter] = 0;
             }
         }
-        index = 1;
-        genSquare(square, currentSquare, index = 1);
-        // generate the magic sqaure
     }
 
     /**
-     * Insert function description.
+     * This function returns true or false for whether
+     * or not array is a magic square.
      *
      * @param preSquare passed in
+     * @return if all possible combinations equal 15
      */
     public static boolean isMagic(final int[] preSquare) {
-        // returns true or false for whether or not array is a magic square
+        // initializing variables that determine sum for combinations
         final int row1 = preSquare[0] + preSquare[1] + preSquare[2];
         final int row2 = preSquare[THREE] + preSquare[FOUR] + preSquare[FIVE];
         final int row3 = preSquare[SIX] + preSquare[SEVEN] + preSquare[EIGHT];
@@ -105,28 +101,34 @@ class MagicSquares {
         final int diag1 = preSquare[0] + preSquare[FOUR] + preSquare[EIGHT];
         final int diag2 = preSquare[2] + preSquare[FOUR] + preSquare[SIX];
 
+        // copies results back to genSquare function
         return row1 == MAGICNUM && row2 == MAGICNUM && row3 == MAGICNUM
-               && col1 == MAGICNUM && col2 == MAGICNUM
-               && col3 == MAGICNUM && diag1 == MAGICNUM && diag2 == MAGICNUM;
+              && col1 == MAGICNUM && col2 == MAGICNUM
+              && col3 == MAGICNUM && diag1 == MAGICNUM && diag2 == MAGICNUM;
     }
 
     /**
-     * Insert function description.
+     * Prints inputted array in a magic square format.
      *
      * @param outputSquare passed in
      */
     public static void printMagicSquare(final int[] outputSquare) {
-        // prints inputted array in a magic square format
-        System.out.println("\n*****");
+        // declaring variables
+        final String border = "\n*****";
+        final String space = " ";
+
+        System.out.println(border);
+
+        // loops through each index in each array to format
         for (int count = 0; count < outputSquare.length; count++) {
             if (count == THREE || count == SIX) {
                 System.out.println();
-                System.out.print(outputSquare[count] + " ");
+                System.out.print(outputSquare[count] + space);
             } else {
-                System.out.print(outputSquare[count] + " ");
+                System.out.print(outputSquare[count] + space);
             }
         }
-        System.out.println("\n*****");
+        System.out.println(border);
     }
 
     /**
@@ -135,11 +137,15 @@ class MagicSquares {
      * @param args nothing passed in
      */
     public static void main(final String[] args) {
-        // main stub, get user input here
-        final int[] magicSquare = {1, 2, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE};
+        // initializing arrays
+        final int[] magicSquare = {1, 2, THREE, FOUR, FIVE,
+            SIX, SEVEN, EIGHT, NINE};
         final int[] extraArray = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-        System.out.println("\n");
-        System.out.println("All Possible Magic Squares (3x3):\n");
-        genSquare(magicSquare, extraArray, 1);
+
+        System.out.println();
+        System.out.println("All Possible Magic Squares (3x3):");
+
+        // function call to generate magic sqare
+        genSquare(magicSquare, extraArray, 0);
     }
 }
